3f555ae00e5a12844773fb4c1bc6e0aa63068719
K&L <37118190+millionairesdigest@users.noreply.github.com>
2018-09-12 19:51:03 -0400

    Update buddyverified.php

--- buddyverified.php
+++ buddyverified.php
@@ -1,39 +1,11 @@
 <?php
-/**
- * Plugin Name: BuddyVerified
- * Plugin URI:  https://wordpress.org/plugins/buddypress-verified/
- * Description: Allows admins to specify verified accounts. Adds a badge to verified user avatars.
- * Version:     2.4.1
- * Author:      modemlooper
- * Author URI:  http://modemlooper.me
- * Donate link: https://wordpress.org/plugins/buddypress-verified/
- * License:     GPLv2
- * Text Domain: buddyverified
- * Domain Path: /languages
- *
- * @link https://wordpress.org/plugins/buddypress-verified/
- *
- * @package BuddyVerified
- * @version 2.4.0
- */
-
-/**
- * Copyright (c) 2017 modemlooper (email : modemlooper@gmail.com)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2 or, at
- * your discretion, any later version, as published by the Free
- * Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
+/*
+Plugin Name: Millionaire's Digest Verified Accounts
+Description: Allow accounts to be verified by displaying a check mark next to their display name. (Set and controlled by the Founder & CEO of the Millionaire's Digest.)
+Version: 1.0.0
+Author: K&L (Founder of the Millionaire's Digest)
+Author URI: https://millionairedigest.com/
+*/
 
 /**
  * Autoloads files with classes when needed
